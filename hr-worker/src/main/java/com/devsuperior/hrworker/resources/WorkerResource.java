a88442a8f43212d977c709acfa1a59a9e1f8bf3c
Welington Lisboa <welington.lisboa@prodeb.ba.gov.br>
2021-04-21 22:14:26 -0300

    hr-worker implementation

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,9 @@ public class WorkerResource {
 		return ResponseEntity.ok(list);
 	}
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		Worker obj = repository.findById(id).get();
+		return ResponseEntity.ok(obj);
+	}
 }
