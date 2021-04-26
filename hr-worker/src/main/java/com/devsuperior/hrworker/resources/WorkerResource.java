9bbc2ae316c400103331bfdf6db8c9d1d2f0480e
Welington Lisboa <welington.lisboa@prodeb.ba.gov.br>
2021-04-26 14:58:33 -0300

    hr-worker Postgress dev profile

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -24,9 +24,6 @@ public class WorkerResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
-	
 	@Autowired
 	private Environment env;
 	
@@ -35,7 +32,7 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs() {
-		logger.info("CONFIG = "+testConfig);
+		//logger.info("CONFIG = "+testConfig);
 		return ResponseEntity.noContent().build();
 	}	
 	
